64afc9615fd3e914e268bec8c8ae49a69737b15c
unknown <santhoshkumar709488@gmail.com>
2026-03-23 17:20:28 +0530

    folders created

---
+++ templeate.sh
@@ -0,0 +1,22 @@
+# creating directory
+mkdir -p src
+mkdir -p research
+
+
+# creating files
+
+touch src/components.py
+touch src/utils.py
+touch src/config.py
+touch src/pipeline.py
+touch src/entity.py
+touch src/constants.py
+touch src/helper.py
+touch src/__init__.py
+touch .env
+touch dvc.yaml
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo " files created successfully"
