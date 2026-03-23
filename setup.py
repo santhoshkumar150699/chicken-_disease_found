e16302e646a00f6e9075e2670abba2595d6ada95
unknown <santhoshkumar709488@gmail.com>
2026-03-23 18:08:42 +0530

    structured

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import  find_packages ,setup
+
+setup (
+    name = "CHICKEN_DISEASE_FOUND"
+    packages=find_packages(),
+    install_requires=[]
+    )
--- templeate.sh
+++ templeate.sh
@@ -1,23 +1,32 @@
 # creating directory
 mkdir -p src
 mkdir -p research
+mkdir -p components 
+mkdir -p utils
+mkdir -p entity
+mkdir -p config
+mkdir -p constants
+mkdir -p templates
+mkdir -p pipeline
 
 
 
 # creating files
 
-touch src/components/__init__.py
-touch src/utils/__init__.py
-touch src/config/__init__.py
-touch src/pipeline/__init__.py
-touch src/entity/__init__.py
-touch src/constants/__init__.py
+
+touch utils/__init__.py
+touch config/__init__.py
+touch pipeline/__init__.py
+touch entity/__init__.py
+touch constants/__init__.py
 touch src/__init__.py
 touch .env
-touch src/config/config.yaml
+touch config/config.yaml
 touch dvc.yaml
+touch params.yaml
 touch setup.py
 touch research/trials.ipynb
 touch requirements.txt
+touch templates/templates.html
 
 echo " files created successfully"
